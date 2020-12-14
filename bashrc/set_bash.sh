b136a2d5c5aa9c70a81e87473202fbbcddda606b
Matt McCarthy <matttmccarthy66@gmail.com>
2020-12-14 11:06:12 -0500

    Enable vi mode

--- bashrc/set_bash.sh
+++ bashrc/set_bash.sh
@@ -1,3 +1,4 @@
 cp $HOME/github/configs/bashrc/bashrc $HOME/.bashrc
 cp $HOME/github/configs/bashrc/profile $HOME/.bash_profile
+cp $HOME/github/configs/bashrc/inputrc $HOME/.inputrc
 source $HOME/.bashrc
