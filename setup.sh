387c370b0a6256b065795854ef58b691d73292f7
Matt McCarthy <matttmccarthy66@gmail.com>
2021-06-03 19:51:53 -0400

    Add alacritty demnu and xterm

--- setup.sh
+++ setup.sh
@@ -16,6 +16,8 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash /dev/stdin -y
 
 # Vim
 apt install -y neovim
+apt install -y dmenu
+apt install -y xterm
 
 # Conda
 CONDA_FILE=Anaconda3-2021.05-Linux-x86_64.sh
@@ -29,3 +31,8 @@ apt install -y powerline powerline-gitstatus
 mkdir -p $HOMEDIR/.xmonad $HOMEDIR/.config/xmobar/
 $HOMEDIR/github/configs/xmonad/setup_xmonad_config.sh
 $HOMEDIR/github/configs/bashrc/set_bash.sh
+
+
+apt-get install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
+source $HOMEDIR/.cargo/bin
+cargo install alacritty
