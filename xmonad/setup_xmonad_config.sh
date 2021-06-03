98713720bf6a323575e11518268cfe8fc0a3323a
Matt McCarthy <matttmccarthy66@gmail.com>
2021-06-03 13:39:07 -0400

    Add xsession as well

--- xmonad/setup_xmonad_config.sh
+++ xmonad/setup_xmonad_config.sh
@@ -4,3 +4,4 @@ cp $XMONADPATH/xmonad.hs $HOME/.xmonad/xmonad.hs
 cp $XMONADPATH/xmobarrc $HOME/.config/xmobar/xmobarrc
 cp $XMONADPATH/gnome_session /usr/share/gnome-session/sessions/xmonad.session
 cp $XMONADPATH/xmonad.desktop /usr/share/applications/xmonad.desktop
+cp $XMONADPATH/xmonad-xsession.desktop /usr/share/xsessions/xmonad-gnome-session.desktop
