b6da60171a07257a8896d42b9e1fff4e6552b9df
Matt McCarthy <matttmccarthy66@gmail.com>
2021-06-03 13:16:37 -0400

    Add gnome session and desktop

--- xmonad/setup_xmonad_config.sh
+++ xmonad/setup_xmonad_config.sh
@@ -1,2 +1,6 @@
-cp $HOME/github/configs/xmonad/xmonad.hs $HOME/.xmonad/xmonad.hs
-cp $HOME/github/configs/xmonad/xmobarrc $HOME/.config/xmobar/xmobarrc
+HOMEDIR=$1
+XMONADPATH=$HOMEDIR/github/configs/xmonad
+cp $XMONADPATH/xmonad.hs $HOME/.xmonad/xmonad.hs
+cp $XMONADPATH/xmobarrc $HOME/.config/xmobar/xmobarrc
+cp $XMONADPATH/gnome_session /usr/share/gnome-session/sessions/xmonad.session
+cp $XMONADPATH/xmonad.desktop /usr/share/applications/xmonad.desktop
